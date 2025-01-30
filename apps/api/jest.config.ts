b3a82c725ee6eba1c7963fc65435ca5c21b0c3de
lharti <7027812+lharti@users.noreply.github.com>
2025-01-30 14:31:34 +0100

    chore(deps): add jest

---
+++ apps/api/jest.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from 'jest'
+
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
+const config: Config = {
+    preset: 'ts-jest',
+
+    clearMocks: true,
+
+    transform: {
+        '^.+\\.(t|j)s$': [
+            'ts-jest',
+            {
+                useESM: true,
+                tsconfig: 'tsconfig.json',
+            },
+        ],
+    },
+
+    collectCoverageFrom: ['src/**/*.(t|j)s'],
+
+    coverageDirectory: '<rootDir>/coverage',
+
+    testEnvironment: 'node',
+
+    prettierPath: require.resolve('prettier-2'),
+
+    roots: ['<rootDir>'],
+
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+    modulePaths: [compilerOptions.baseUrl],
+
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/',
+    }),
+}
+
+export default config
