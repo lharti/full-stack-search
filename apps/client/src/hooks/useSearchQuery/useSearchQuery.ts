45e95dc91d49bb0abcce3bfa25b4c67e726c6f09
lharti <7027812+lharti@users.noreply.github.com>
2025-02-05 03:33:00 +0100

    fix(client): don't retry search query if request failed

--- apps/client/src/hooks/useSearchQuery/useSearchQuery.ts
+++ apps/client/src/hooks/useSearchQuery/useSearchQuery.ts
@@ -6,18 +6,20 @@ export const useSearchQuery = (query: string) => {
     const searchQuery = useQuery<SearchResult>({
         queryKey: ['search', query],
 
-        queryFn: async () => {
-            const response = await apiClient.get(`/search?q=${query}`)
+        queryFn: async () => fetchSearchResults(query),
 
-            if (!response.ok) {
-                throw new Error(
-                    'An error occurred while fetching the search results',
-                )
-            }
-
-            return response.json()
-        },
+        retry: false,
     })
 
     return searchQuery
+}
+
+const fetchSearchResults = async (query: string) => {
+    const response = await apiClient.get(`/search?q=${query}`)
+
+    if (!response.ok) {
+        throw new Error('An error occurred while fetching the search results')
+    }
+
+    return response.json() as Promise<SearchResult>
 }
